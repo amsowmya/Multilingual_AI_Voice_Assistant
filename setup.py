888a9997cc061f9f9ee03d2a5bfa738c42b96133
amsowmya <soumya.pdit@gmail.com>
2025-02-07 21:38:10 +0530

    added required files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="genai AI Project",
+    version="0.1.1",
+    author="Sowmya AM",
+    author_email="sowmya.anekonda@gmail.com",
+    packages=find_packages()
+)
